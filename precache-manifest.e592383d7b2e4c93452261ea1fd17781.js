37458cd24b0023f4f5289af67e9c18d1993fcb86
Code Sandbox <hello@codesandbox.io>
2021-03-26 18:01:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e592383d7b2e4c93452261ea1fd17781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdb3bbf96a8bc628fab5edb9f4288b2",
+    "url": "https://thomasrosen.github.io/csb-p1spx/index.html"
+  },
+  {
+    "revision": "c463fddd5ca750c106b7",
+    "url": "https://thomasrosen.github.io/csb-p1spx/static/css/main.bec094ea.chunk.css"
+  },
+  {
+    "revision": "5f62a2fa05c725df508b",
+    "url": "https://thomasrosen.github.io/csb-p1spx/static/js/2.d164d6cc.chunk.js"
+  },
+  {
+    "revision": "c463fddd5ca750c106b7",
+    "url": "https://thomasrosen.github.io/csb-p1spx/static/js/main.8ffd23ec.chunk.js"
+  },
+  {
+    "revision": "317da1a184e3e5962e29",
+    "url": "https://thomasrosen.github.io/csb-p1spx/static/js/runtime~main.09c4bb5a.js"
+  }
+]);
